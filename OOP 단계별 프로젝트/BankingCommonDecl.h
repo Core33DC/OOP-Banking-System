4bb9df3cb33e70b9193e101f97db44ece7e96c2b
Core33DC <ning0790@gmail.com>
2022-03-24 14:37:55 +0900

    Ver 0.8 업데이트

---
+++ OOP 단계별 프로젝트/BankingCommonDecl.h
@@ -0,0 +1,27 @@
+//파일이름: AccountHandler.cpp
+//작성자: 이진우
+//업데이트 정보: [2022. 03. 22] 파일버전 0.7
+//내용 : Class별로 파일분할 -> 은행계좌관리프로그램2 프로젝트로 다시 작성
+
+#ifndef __BANKING_COMMON_H__
+#define __BANKING_COMMON_H__
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int NAME_LEN = 20;
+
+//프로그램 사용자의 선택 메뉴
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+//신용등급 추가
+enum { LEVEL_A = 7, LEVEL_B = 4, LEVEL_C = 2 };
+
+//계좌의 종류 추가
+enum { NORMAL = 1, CREDIT = 2 };
+
+#endif // !__BANKING_COMMON_H__
+
+
---
+++ OOP 단계별 프로젝트/BankingSystemMain.cpp
@@ -0,0 +1,43 @@
+//소프트웨어 버전: Banking System Ver 0.7
+//파일이름: BankingSystemMain.cpp
+//작성자: 이진우
+//업데이트 정보: [2022. 03. 22] 파일버전 0.7
+//내용 : Class별로 파일분할 -> 은행계좌관리프로그램2 프로젝트로 다시 작성
+
+#include "BankingCommonDecl.h"
+#include "AccountHandler.h"
+
+int main(void)
+{
+	AccountHandler manager;
+	int choice;
+
+	while (1)
+	{
+		manager.ShowMenu();
+		cout << "선택: ";
+		cin >> choice;
+		cout << endl;
+
+		switch (choice)
+		{
+		case MAKE:
+			manager.MakeAccount();
+			break;
+		case DEPOSIT:
+			manager.DepositMoney();
+			break;
+		case WITHDRAW:
+			manager.WithdrawMoney();
+			break;
+		case INQUIRE:
+			manager.ShowAllAccInfo();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection.." << endl;
+		}
+	}
+	return 0;
+}
